2e8cca79feffa16e494eb0a9365f691c0cf628ef
MUQARRAB HUSSAIN <muqarrabhussain469@gmail.com>
2023-07-10 14:24:19 +0500

    Drawer Making

---
+++ app/src/main/java/com/example/mobileapplicationassignment/ActivityMainBinding.java
@@ -0,0 +1,9 @@
+package com.example.mobileapplicationassignment;
+
+import android.view.LayoutInflater;
+
+public class ActivityMainBinding {
+    public static ActivityMainBinding inflate(LayoutInflater layoutInflater) {
+
+    }
+}
---
+++ app/src/main/java/com/example/mobileapplicationassignment/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.mobileapplicationassignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(R.layout.activity_main);
+
+    }
+}
